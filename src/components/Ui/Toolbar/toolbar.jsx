ab3a5c412a59150345b40b9abe95239f2bd86e18
Janith Malshan <janithmalshan@gmail.com>
2020-08-04 00:01:47 +0300

    Tools and window

--- src/components/Ui/Toolbar/toolbar.jsx
+++ src/components/Ui/Toolbar/toolbar.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import './toolbar.scss'
+import './toolbar.scss';
+import ic1 from '../../../assets/icons/ic-tool-01.svg';
+import ic2 from '../../../assets/icons/ic-tool-02.svg';
+import ic3 from '../../../assets/icons/ic-tool-03.svg';
+import ic4 from '../../../assets/icons/ic-tool-04.svg';
+import ic5 from '../../../assets/icons/ic-tool-05.svg';
+import ic6 from '../../../assets/icons/ic-tool-06.svg';
+import ic7 from '../../../assets/icons/ic-tool-07.svg';
+import ic8 from '../../../assets/icons/ic-tool-08.svg';
+import ic9 from '../../../assets/icons/ic-tool-09.svg'
 
 export default function Toolbar() {
 
     return (
         <div className="ui-toolbar">
+            <ToolItem toolicon={ic1} toolhint="#$&*"/>
+            <ToolItem toolicon={ic2} toolhint="#$&*"/>
+            <ToolItem toolicon={ic3} toolhint="#$&*"/>
+            <ToolItem toolicon={ic4} toolhint="#$&*"/>
+            <ToolItem toolicon={ic5} toolhint="#$&*"/>
+            <ToolItem toolicon={ic6} toolhint="#$&*"/>
+            <ToolItem toolicon={ic7} toolhint="#$&*"/>
+            <ToolItem toolicon={ic8} toolhint="#$&*"/>
+            <ToolItem toolicon={ic9} toolhint="#$&*"/>
         </div>
     );
 }
+
+const ToolItem = (props) => {
+    return (
+        <div className="ui-toolbar__item">
+            <img className="ui-toolbar__item-icon" src={props.toolicon} alt="Prestau"/>
+            {/*<h3 className="ui-why-item__title">{props.txtTitle}</h3>*/}
+            <p className="ui-why-item__desc">{props.toolhint}</p>
+        </div>
+    )
+};
--- src/components/Ui/Window/window.jsx
+++ src/components/Ui/Window/window.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import './window.scss'
+import back from '../../../assets/ic-w-back.svg'
+import icon from '../../../logo.svg'
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: '#f0f0f0',
+        padding: '14px 0',
+        width: 48,
+        height: 48,
+        boxSizing: 'border-box',
+        position: 'absolute',
+        top: 24,
+        left: 24,
+        zIndex: 99,
+
+        '&:hover': {
+            backgroundColor: '#c0c0c0',
+            boxShadow: 'none',
+        },
+    },
+}));
 
 export default function Window() {
+    const classes = useStyles();
 
     return (
         <div className="ui-window">
+            <IconButton className={classes.root} component="a">
+                <img src={back} alt="back"/>
+            </IconButton>
+
+            <Scaler>
+                <img src={icon} height={300}/>
+            </Scaler>
         </div>
     );
 }
+
+const Scaler = (props) => {
+    return (
+        <div className="ui-window__scale">
+            <div className="ui-window__scale-top"/>
+            <div className="ui-window__scale-right"/>
+            <div className="ui-window__scale-bottom"/>
+            <div className="ui-window__scale-left"/>
+            {props.children}
+        </div>
+    )
+};
