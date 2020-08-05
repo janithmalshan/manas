107930195681ffd711f87d20e0a35ffa18fd7cbd
Janith Malshan <janithmalshan@gmail.com>
2020-08-05 14:30:25 +0300

    Home page options section

--- src/components/Ui/Options/options.jsx
+++ src/components/Ui/Options/options.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {ReactPropTypes as PropTypes} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import './options.scss'
 import icon from "../../../assets/ic-expand.svg";
+import plus from "../../../assets/ic-panel-plus.svg";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from '@material-ui/core/styles';
+import { ChromePicker } from 'react-color';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
         height: 44,
         boxSizing: 'border-box',
         position: 'absolute',
-        top: 24,
+        top: 10,
         left: 24,
         zIndex: 99,
     },
@@ -20,12 +29,122 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Options() {
     const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const [expanded, setExpanded] = React.useState('panel3');
+
+    const handleChange = (panel) => (event, newExpanded) => {
+        setExpanded(newExpanded ? panel : false);
+    };
+
+    const handleChangeTab = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <div className="ui-option">
             <IconButton className={classes.root} component="a">
                 <img src={icon} alt="back"/>
             </IconButton>
+            <div className="ui-option__tabs">
+                <Tabs value={value} onChange={handleChangeTab} aria-label="simple tabs example">
+                    <Tab label="Settings"/>
+                    <Tab label="Objects"/>
+                </Tabs>
+            </div>
+            <TabPanel value={value} index={0}>
+                <div>
+                    <MuiAccordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')} elevation={0}>
+                        <MuiAccordionSummary aria-controls="panel1d-content" id="panel1d-header" expandIcon={<img src={plus}/>}>
+                            <Typography>Fills</Typography>
+                        </MuiAccordionSummary>
+                        <MuiAccordionDetails>
+                        </MuiAccordionDetails>
+                    </MuiAccordion>
+                    <MuiAccordion square expanded={expanded === 'panel2'} onChange={handleChange('panel2')} elevation={0}>
+                        <MuiAccordionSummary aria-controls="panel2d-content" id="panel2d-header" expandIcon={<img src={plus}/>}>
+                            <Typography>Stroke</Typography>
+                        </MuiAccordionSummary>
+                        <MuiAccordionDetails>
+                        </MuiAccordionDetails>
+                    </MuiAccordion>
+                    <MuiAccordion square expanded={expanded === 'panel3'} onChange={handleChange('panel3')} elevation={0}>
+                        <MuiAccordionSummary aria-controls="panel3d-content" id="panel3d-header" expandIcon={<img src={plus}/>}>
+                            <Typography>Color</Typography>
+                        </MuiAccordionSummary>
+                        <MuiAccordionDetails>
+                            <div className="ui-option__panel-picker">
+                                <ColorPick />
+                            </div>
+                        </MuiAccordionDetails>
+                    </MuiAccordion>
+                    <MuiAccordion square expanded={expanded === 'panel4'} onChange={handleChange('panel4')} elevation={0}>
+                        <MuiAccordionSummary aria-controls="panel4d-content" id="panel4d-header" expandIcon={<img src={plus}/>}>
+                            <Typography>Opacity</Typography>
+                        </MuiAccordionSummary>
+                        <MuiAccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </MuiAccordionDetails>
+                    </MuiAccordion>
+                    <MuiAccordion square expanded={expanded === 'panel5'} onChange={handleChange('panel5')} elevation={0}>
+                        <MuiAccordionSummary aria-controls="panel5d-content" id="panel5d-header" expandIcon={<img src={plus}/>}>
+                            <Typography>X</Typography>
+                        </MuiAccordionSummary>
+                        <MuiAccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </MuiAccordionDetails>
+                    </MuiAccordion>
+                    <MuiAccordion square expanded={expanded === 'panel6'} onChange={handleChange('panel6')} elevation={0}>
+                        <MuiAccordionSummary aria-controls="panel6d-content" id="panel6d-header" expandIcon={<img src={plus}/>}>
+                            <Typography>Y</Typography>
+                        </MuiAccordionSummary>
+                        <MuiAccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                                elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </MuiAccordionDetails>
+                    </MuiAccordion>
+                </div>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+               Objects
+            </TabPanel>
         </div>
     );
+}
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+            className="ui-option__panel"
+        >
+            {value === index && (
+                <div>
+                    <h2>{children}</h2>
+                </div>
+            )}
+        </div>
+    );
+}
+
+const ColorPick = () => {
+    return (
+        <ChromePicker />
+    )
 }
